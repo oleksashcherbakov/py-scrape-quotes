afc50aa2c2777a7a8ce257ec1d229f41273f9fc0
kushik <lada.chokol@gmail.com>
2025-09-08 20:32:51 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -26,7 +26,10 @@ def get_one_quote(quote_html: Tag) -> Quote:
     author = author_el.get_text(strip=True) if author_el else ""
 
     tags_container = quote_html.select_one(".tags")
-    tag_elements = tags_container.find_all("a", class_="tag") if tags_container else []
+    if tags_container:
+        tag_elements = tags_container.find_all("a", class_="tag")
+    else:
+        tag_elements = []
     tags_list = [element.get_text(strip=True) for element in tag_elements]
 
     return Quote(text, author, tags_list)
